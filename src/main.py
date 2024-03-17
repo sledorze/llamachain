78000dce5ca8fef8e60e891c48cb58c3c7201056
Rayed Bin Wahed <rayed@therapservices.net>
2024-03-17 12:10:12 +0800

    Use chatollama and fix imports

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 import os
 
-from langchain_community.llms.ollama import Ollama
+from langchain_community.chat_models.ollama import ChatOllama 
 
-llama_model = Ollama(model="llama2", base_url=os.environ["OLLAMA_BASE_URL"])
+llama_model = ChatOllama(model="llama2", base_url=os.environ["OLLAMA_BASE_URL"])
 
 query = "Why is the sky blue?"
 
 for chunk in llama_model.stream(query):
-    print(chunk, end="", flush=True)
+    print(chunk.content, end="", flush=True)
